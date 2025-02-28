406114c031bf68ff05a785587e68790bb2f8dd2f
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-28 12:13:29 +0100

    add ajax call from API

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('ciao');
+//recover nodes from dom
+const rowEl = document.querySelector('.row');
+const postUrlEndpoint = 'https://lanciweb.github.io/demo/api/pictures/';
+
+
+fetch(postUrlEndpoint)
+    .then(res => res.json())
+    .then(posts => {
+        console.log(posts);
+        
+    })
