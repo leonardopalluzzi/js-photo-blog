1643d586cc5fe9686a271b09de2ef6a276b777c1
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-28 12:16:41 +0100

    add markup function

--- main.js
+++ main.js
@@ -9,3 +9,26 @@ fetch(postUrlEndpoint)
         console.log(posts);
         
     })
+
+
+
+ 
+
+
+
+function getMarkup(post) {
+    return `    <div class="col-12 col-md-6 col-lg-4">
+                    <div class="custom_card">
+                        <img src="" alt="pin" class="pin">
+                        <div class="card-header">
+                            <img src="${post.url}" alt="main img" class="card_img">
+                        </div>
+                        <div class="card-body">
+                            <p class="photo_data sometype-mono-font">${post.date}</p>
+                            <h3 class="photo_title edu-tas-beginner-font">
+                                ${post.title.toUpperCase()}
+                            </h3>
+                        </div>
+                    </div>
+                </div>`
+}
